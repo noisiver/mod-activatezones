afe348c6e236b9ed4b3314361af1232bb2448b58
Revision <tkn963@gmail.com>
2021-12-18 01:50:52 +0100

    Fixes
    Fixed many dumb mistakes that I made because I'm an idiot.
    
    I hopefully made the module work as it was intended originally.

--- src/ActivateZone.cpp
+++ src/ActivateZone.cpp
@@ -7,9 +7,12 @@ class ActivateZone : public AllCreatureScript
 
         void OnAllCreatureUpdate(Creature* creature, uint32 diff) override
         {
-            if (!sWorld->getBoolConfig(CONFIG_SET_ALL_CREATURES_WITH_WAYPOINT_MOVEMENT_ACTIVE))
+            if (!sWorld->getBoolConfig(CONFIG_SET_ALL_CREATURES_WITH_WAYPOINT_MOVEMENT_ACTIVE) && sWorld->getBoolConfig(CONFIG_PRELOAD_ALL_NON_INSTANCED_MAP_GRIDS))
             {
                 if (creature->GetMapId() != 0 && creature->GetMapId() != 1 && creature->GetMapId() != 530 && creature->GetMapId() != 571)
+                    return;
+
+                if (creature->IsSummon() || creature->IsPet() || creature->IsHunterPet() || creature->IsTotem())
                     return;
 
                 CreatureAddon const* cainfo = creature->GetCreatureAddon();
@@ -21,26 +24,27 @@ class ActivateZone : public AllCreatureScript
                     if (time > delay)
                     {
                         if (cainfo->path_id != 0)
-                        {
                             creature->setActive(true);
-                        }
+
+                        time = 0;
                     }
                 }
                 else
                 {
-                    if (cainfo->path_id != 0)
+                    if (time > delay)
                     {
-                        creature->setActive(false);
+                        if (cainfo->path_id != 0)
+                            creature->setActive(false);
+
+                        time = 0;
                     }
                 }
-
-                time = 0;
             }
         }
 
     private:
         uint32 time = 0;
-        uint32 delay = 60 * 1000;
+        uint32 delay = 1 * (60 * 1000);
 };
 
 void AddActivateZonesScripts()
